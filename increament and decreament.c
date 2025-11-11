33f555e20efdc3c4b455448943b7f49ad80e49cd
roopsricharangogada <roopsricharan.gogada@gmail.com>
2025-11-11 14:21:55 +0530

    Add files via upload

---
+++ increament and decreament.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,result,i,j;
+	scanf("%d%d%d%d%d",&a,&b,&c,&d,&i);
+	result=(a/b*c-b+a*d/3);
+	j=(i++)+(++i);
+	printf("Result:%d\n",result);
+	printf("j=%d",j);
+	return 0;
+}
